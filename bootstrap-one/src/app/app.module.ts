6fbffa969a8e6ada7811406bfe7bdb0fb1f0b0e5
sadakpramodh <sadakpramodh@live.com>
2019-02-13 15:41:00 +0530

    completed up to routing

--- bootstrap-one/src/app/app.module.ts
+++ bootstrap-one/src/app/app.module.ts
@@ -1,3 +1,11 @@
+import { AdminAuthGuard } from './services/admin-auth-guard.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthHttp, AUTH_PROVIDERS, provideAuth, AuthConfig } from 'angular2-jwt/angular2-jwt';
+import { OrderService } from './services/order.service';
+import { MockBackend } from '@angular/http/testing';
+import { fakeBackendProvider } from './helpers/fake-backend';
+import { AuthService } from './services/auth.service';
+
 import { RouterModule } from '@angular/router';
 import { GithubFollowersService } from './github-followers.service';
 import { AppErrorHandler } from './services/app-error-handler';
@@ -5,7 +13,7 @@ import { PostService } from './services/post.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, BaseRequestOptions } from '@angular/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +33,11 @@ import { HomeComponent } from './home/home.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { NoAccessComponent } from './no-access/no-access.component';
+
 
 @NgModule({
   declarations: [
@@ -43,7 +56,13 @@ import { NavbarComponent } from './navbar/navbar.component';
     HomeComponent,
     GithubProfileComponent,
     NotFoundComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    AdminComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NoAccessComponent
   ],
   imports: [
     BrowserModule,
@@ -56,6 +75,9 @@ import { NavbarComponent } from './navbar/navbar.component';
       { path: 'followers', component: GithubFollowersComponent },
       { path: 'profile/:username', component: GithubProfileComponent },
       { path: 'posts', component: HttpJsonSampleComponent},
+      { path: 'admin', component: AdminComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+      { path: 'login', component: LoginComponent },
+      { path: 'no-access', component: NoAccessComponent },
       { path: '**', component: NotFoundComponent}
 
     ])
@@ -63,8 +85,19 @@ import { NavbarComponent } from './navbar/navbar.component';
   providers: [
     PostService,
     GithubFollowersService,
-    { provide: ErrorHandler, useClass: AppErrorHandler }
+    { provide: ErrorHandler, useClass: AppErrorHandler },
+    OrderService,
+
+    AuthService,
+    AuthGuard,
+    AdminAuthGuard,
+
+    // For creating a mock back-end. You don't need these in a real app. 
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
   ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ bootstrap-one/src/app/services/auth-guard.service.ts
@@ -0,0 +1,21 @@
+import { AuthService } from './auth.service';
+
+import { Router, CanActivate, RouterStateSnapshot } from '@angular/router';
+
+
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) { }
+  canActivate(route, state: RouterStateSnapshot) {
+    if (this.authService.isLoggedIn()) return true;
+
+    this.router.navigate(['/login'], { queryParams: {returnUrl: state.url}});
+    return false;
+  }
+}
